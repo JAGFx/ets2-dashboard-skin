cabac09216681334e642f7f2d5da2b6375a59d4c
JAGFx <contact@jagfx.fr>
2020-04-05 22:20:41 +0200

    :heavy_plus_sign: add server inside directly

--- old/README.md
+++
@@ -1,5 +0,0 @@
-# ets2-dashboard-skin
-
-Use this [ETS2 Telemetry server](https://github.com/Funbit/ets2-telemetry-server)
-
-![Dashboard](dashboard.jpg)
---
+++ server/gulpfile.babel.js
@@ -0,0 +1,19 @@
+import gulp    from 'gulp';
+import babel   from 'gulp-babel';
+import plumber from 'gulp-plumber';
+
+function watch() {
+	gulp.watch( 'index.js', { cwd: './' }, gulp.series( build ) );
+}
+
+function build() {
+	return gulp.src( 'index.js' )
+			   .pipe( plumber() )
+			   .pipe( babel( { presets: [ '@babel/env' ] } ) )
+			   .pipe( gulp.dest( 'dist' ) );
+}
+
+exports.build = build;
+exports.watch = watch;
+
+exports.default = gulp.series( build, watch );
