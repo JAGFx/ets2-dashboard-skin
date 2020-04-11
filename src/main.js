6eace10ec001fab603e55f910498e226ce28edf0
JAGFx <contact@jagfx.fr>
2020-04-11 15:06:03 +0200

    🐛 fix port for server connexion

--- src/main.js
+++ src/main.js
@@ -1,18 +1,19 @@
-import Vue         from 'vue'
-import VueSocketIO from 'vue-socket.io'
-import App         from './App.vue'
+import Vue         from 'vue';
+import VueSocketIO from 'vue-socket.io';
+import App         from './App.vue';
 
-window.$ = require('jquery');
-window.JQuery = require('jquery');
-window.NoSleep = require('nosleep.js');
+window.$          = require( 'jquery' );
+window.JQuery     = require( 'jquery' );
+window.NoSleep    = require( 'nosleep.js' );
+window.animateCss = require( 'animate.css' );
 
-Vue.use(new VueSocketIO({
-    debug:      false,
-    connection: "http://" + window.location.host,
-}));
+Vue.use( new VueSocketIO( {
+	debug:      false,
+	connection: 'http://' + window.location.hostname + ':3000'
+} ) );
 
 Vue.config.productionTip = false;
 
-new Vue({
-  render: h => h(App),
-}).$mount("#app");
+new Vue( {
+	render: h => h( App )
+} ).$mount( '#app' );
