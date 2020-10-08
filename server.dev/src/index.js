fd682aaee14c53bfbc7bf302cb4992b9556b20e8
JAGFx <contact@jagfx.fr>
2020-10-08 20:25:21 +0200

    :sparkles: add socket server dev

---
+++ server.dev/src/index.js
@@ -0,0 +1,34 @@
+/**
+ * @author:	Emmanuel SMITH <hey@emmanuel-smith.me>
+ * project:	ets2-dashboard-skin
+ * file: 	index.js
+ * Date: 	08/10/2020
+ * Time: 	20:02
+ */
+
+const path     = require( 'path' );
+const socketio = require( 'socket.io' );
+
+const options = { /* ... */ };
+const io      = socketio( options );
+const data    = path.resolve( process.cwd(), '../src/data/scs_sdk_plugin_parsed_data.json' );
+const port    = 3000;
+
+io.listen( port, () => {
+	const url       = `localhost:${ port }`;
+	const data      = {
+		url:  url,
+		port: port
+	};
+	const eventName = 'server.listen';
+	const txt       = `Euro Truck Simulator 2 dashboard is running at http://${ url }/`;
+	io.emit( 'log', {
+		eventName: eventName,
+		rawData:   data
+	} );
+	console.log( `[${ eventName }] ${ txt }` );
+} );
+io.on( 'connection', socket => {
+	io.emit( 'update', data );
+	console.log( 'Update' );
+} );
