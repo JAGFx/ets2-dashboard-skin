ef51b809525405e2cb0ed59097bc84b9c17d5873
JAGFx <contact@jagfx.fr>
2021-11-29 21:46:28 +0100

    :construction: [feat/config] #77 rework server
    Signed-off-by: JAGFx <contact@jagfx.fr>

---
+++ servers/dev/index.js
@@ -0,0 +1,18 @@
+/**
+ * @author:	Emmanuel SMITH <hey@emmanuel-smith.me>
+ * project:	ets2-dashboard-skin
+ * file: 	index.js
+ * Date: 	16/11/2021
+ * Time: 	20:18
+ */
+
+
+import { initApp, initConfig, initServer, initSocket } from 'ets2-dashboard-lib/server.js';
+import path     from 'path';
+
+const dirname = path.resolve();
+
+initApp();
+initConfig( dirname );
+initSocket();
+initServer();
