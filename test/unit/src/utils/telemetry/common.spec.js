1cbd22f616ff8edbe1857d9bd9487bbddb2f100a
JAGFx <contact@jagfx.fr>
2022-02-24 19:56:46 +0100

    :white_check_mark: #99 add unit test for common telemetry
    Signed-off-by: JAGFx <contact@jagfx.fr>

--- test/unit/src/utils/telemetry/common.spec.js
+++ test/unit/src/utils/telemetry/common.spec.js
@@ -1,32 +1,33 @@
 import { $scale } from '@/utils/telemetry/_common.utils';
 
 describe('Telemetry common utils', () => {
-  test.each([
-    {
-      size: {
-        width: 0,
-        height: 720
-      }
-    },
-    {
-      size: {
-        width: 1280,
-        height: 0
-      }
-    },
-    {
-      size: {
-        width: 0,
-        height: 0
-      }
-    }
-  ])(
-    'Test $scale must return 1 without skin width or height',
-    (currentSkin) => {
-      global.innerWidth = 1920;
-      global.innerHeight = 1080;
+  const windowWidth = 1920;
+  const windowHeight = 1080;
 
-      expect($scale(currentSkin)).toBe(1);
-    }
-  );
+  global.innerWidth = windowWidth;
+  global.innerHeight = windowHeight + 41;
+
+  test.each([
+    { size: { width: 2560, height: windowHeight } },
+    { size: { width: 0, height: windowHeight } },
+    { size: { width: 4096, height: 2048 } },
+    { size: { width: windowWidth, height: 2048 } },
+    { size: { width: windowWidth, height: 0 } },
+    { size: { width: windowWidth, height: windowHeight } },
+    { size: { width: 0, height: 0 } }
+  ])('The skin should be reduced', (currentSkin) => {
+    expect($scale(currentSkin)).toBeLessThan(1);
+  });
+
+  test.each([
+    { size: { width: 1280, height: windowHeight } },
+    { size: { width: 0, height: windowHeight } },
+    { size: { width: 1280, height: 720 } },
+    { size: { width: windowWidth, height: 720 } },
+    { size: { width: windowWidth, height: 0 } },
+    { size: { width: windowWidth, height: windowHeight } },
+    { size: { width: 0, height: 0 } }
+  ])('The skin should be enlarged', (currentSkin) => {
+    expect($scale(currentSkin)).toBeGreaterThanOrEqual(1);
+  });
 });
