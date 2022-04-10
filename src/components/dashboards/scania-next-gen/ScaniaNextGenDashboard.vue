8b875938db08357209f70078237b14676a1d8a02
JAGFx <contact@jagfx.fr>
2022-04-10 15:54:23 +0200

    :lipstick: [feat/rework-scania] apply some adjustment on scania next gen skin
    Signed-off-by: JAGFx <contact@jagfx.fr>

--- src/components/dashboards/scania-next-gen/ScaniaNextGenDashboard.vue
+++ src/components/dashboards/scania-next-gen/ScaniaNextGenDashboard.vue
@@ -9,53 +9,53 @@
         height: dashProps.skinData.size.height + 'px'
       }"
     >
-      <ScaniaNextGenGauges />
-      <ScaniaNextGenGaugeDisplay class="left">
-        <div
-          class="speed d-flex w-100 justify-content-end align-items-end mb-2"
-        >
-          <span class="value">{{ telemetry2.truck.speed.toFixed(0) }}</span>
-          <span class="unit">{{ $unitReadable('unit_speed') }}</span>
-        </div>
-        <div class="line d-flex w-100 justify-content-end align-items-end mb-2">
-          <span class="value">{{ telemetry2.truck.odometer.toFixed(0) }}</span>
-          <span class="unit">{{ $unitReadable('unit_length', 'km') }}</span>
-        </div>
-        <div class="line d-flex w-100 justify-content-between align-items-end">
-          <span class="trip-label">Trip</span>
-          <div class="d-flex justify-content-end align-items-end">
-            <span class="value">-</span>
-            <span class="unit">{{ $unitReadable('unit_length', 'km') }}</span>
-          </div>
-        </div>
-      </ScaniaNextGenGaugeDisplay>
-      <ScaniaNextGenGaugeDisplay class="right">
-        <div
-          class="hour d-flex w-100 justify-content-center align-items-end mb-2"
-        >
-          <span class="value">22:35</span>
-        </div>
-        <div
-          class="exterior-temperature d-flex w-100 justify-content-end align-items-end mb-2"
-        >
-          <span class="value">-</span>
-          <span class="unit">{{ $unitReadable('unit_degrees') }}</span>
-        </div>
-        <ScaniaNextGenAdBlueBar />
-      </ScaniaNextGenGaugeDisplay>
-      <Bars
-        class="fuel-gauge"
-        :level="telemetry2.truck.fuelLevel"
-        :capacity="telemetry2.truck.fuelCapacity"
-        :factor="8"
-      />
-      <Bars
-        class="water-temperature-gauge"
-        :level="telemetry2.truck.engineWaterTemperature"
-        :capacity="120"
-        :factor="8"
-        mode="bar"
-      />
+      <!--      <ScaniaNextGenGauges />-->
+      <!--      <ScaniaNextGenGaugeDisplay class="left">-->
+      <!--        <div-->
+      <!--          class="speed d-flex w-100 justify-content-end align-items-end mb-2"-->
+      <!--        >-->
+      <!--          <span class="value">{{ telemetry2.truck.speed.toFixed(0) }}</span>-->
+      <!--          <span class="unit">{{ $unitReadable('unit_speed') }}</span>-->
+      <!--        </div>-->
+      <!--        <div class="line d-flex w-100 justify-content-end align-items-end mb-2">-->
+      <!--          <span class="value">{{ telemetry2.truck.odometer.toFixed(0) }}</span>-->
+      <!--          <span class="unit">{{ $unitReadable('unit_length', 'km') }}</span>-->
+      <!--        </div>-->
+      <!--        <div class="line d-flex w-100 justify-content-between align-items-end">-->
+      <!--          <span class="trip-label">Trip</span>-->
+      <!--          <div class="d-flex justify-content-end align-items-end">-->
+      <!--            <span class="value">-</span>-->
+      <!--            <span class="unit">{{ $unitReadable('unit_length', 'km') }}</span>-->
+      <!--          </div>-->
+      <!--        </div>-->
+      <!--      </ScaniaNextGenGaugeDisplay>-->
+      <!--      <ScaniaNextGenGaugeDisplay class="right">-->
+      <!--        <div-->
+      <!--          class="hour d-flex w-100 justify-content-center align-items-end mb-2"-->
+      <!--        >-->
+      <!--          <span class="value">22:35</span>-->
+      <!--        </div>-->
+      <!--        <div-->
+      <!--          class="exterior-temperature d-flex w-100 justify-content-end align-items-end mb-2"-->
+      <!--        >-->
+      <!--          <span class="value">-</span>-->
+      <!--          <span class="unit">{{ $unitReadable('unit_degrees') }}</span>-->
+      <!--        </div>-->
+      <!--        <ScaniaNextGenAdBlueBar />-->
+      <!--      </ScaniaNextGenGaugeDisplay>-->
+      <!--      <Bars-->
+      <!--        class="fuel-gauge"-->
+      <!--        :level="telemetry2.truck.fuelLevel"-->
+      <!--        :capacity="telemetry2.truck.fuelCapacity"-->
+      <!--        :factor="8"-->
+      <!--      />-->
+      <!--      <Bars-->
+      <!--        class="water-temperature-gauge"-->
+      <!--        :level="telemetry2.truck.engineWaterTemperature"-->
+      <!--        :capacity="120"-->
+      <!--        :factor="8"-->
+      <!--        mode="bar"-->
+      <!--      />-->
 
       <!-- <editor-folder> Symbols -->
       <ScaniaNextGenGeneralWarningSymbols />
