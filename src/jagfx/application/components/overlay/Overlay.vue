7919b07eb378507a1fd04782e2992d9378d29e34
CorentinGS <c.giaufersaubert@outlook.com>
2022-08-22 14:49:22 +0200

    :sparkles: Overlay component

---
+++ src/jagfx/application/components/overlay/Overlay.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="overlay container-fluid">
+    <div class="overlay-content">
+      <Transition mode="out-in" name="fade">
+        <slot></slot>
+      </Transition>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// Defines a closeOverlay event that can be emitted by the overlay component.
+defineEmits(["closeOverlay"]);
+</script>
+
+<style lang="scss">
+@import "overlay";
+</style>
