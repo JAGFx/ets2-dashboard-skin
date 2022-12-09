2ad03820df4950552d0ccde128e6a13e6992f71a
Emmanuel SMITH <emmanuel.smith@jagfx.fr>
2022-12-09 20:33:21 +0100

    :wrench: correct the running application

--- tools/.jestrc.ts
+++ tools/.jestrc.ts
@@ -1,18 +1,25 @@
-import type { Config } from "@jest/types"
-import tsConfig from './tsconfig.json';
+import type { Config }          from "@jest/types"
+import { JestConfigWithTsJest } from 'ts-jest';
+import tsconfig                 from './tsconfig.json';
 
-const config: Config.InitialOptions = {
+const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   clearMocks: true,
   rootDir: '../',
-  modulePaths: [tsConfig.compilerOptions.baseUrl],
+  modulePaths: [ tsconfig.compilerOptions.baseUrl],
   moduleNameMapper: {
     '@/(.*)$': '<rootDir>/src/$1',
     '@core/(.*)$': '<rootDir>/lib/jagfx/core/src/$1',
-    '@server/(.*)$': '<rootDir>/lib/jagfx/server/src/$1',
+    // '@server/(.*)$': '<rootDir>/lib/jagfx/server/src/$1',
   },
   transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tools/tsconfig.json'
+      },
+    ],
     '^.+\\.yaml$': 'yaml-jest'
   },
 };
